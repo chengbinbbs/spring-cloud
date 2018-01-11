ef220af80545921c38554f14cf909508442e1dcb
chengbinbbs <1437243814@qq.com>
2018-01-11 11:12:05 +0800

    Update SpringCloudEurekaApplication.java

--- spring-cloud-eureka/src/main/java/com/chengbinbbs/SpringCloudEurekaApplication.java
+++ spring-cloud-eureka/src/main/java/com/chengbinbbs/SpringCloudEurekaApplication.java
@@ -1,5 +1,7 @@
 package com.chengbinbbs;
 
+import io.prometheus.client.spring.boot.EnablePrometheusEndpoint;
+import io.prometheus.client.spring.boot.EnableSpringBootMetricsCollector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
