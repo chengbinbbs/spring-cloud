473c9fcd1492c3a0729216e5f4cdc45cf6325f02
chengbinbbs <1437243814@qq.com>
2018-01-11 11:11:33 +0800

    eureka高可用注册中心加入权限认证，服务监控

--- spring-cloud-eureka/src/main/java/com/chengbinbbs/SpringCloudEurekaApplication.java
+++ spring-cloud-eureka/src/main/java/com/chengbinbbs/SpringCloudEurekaApplication.java
@@ -6,6 +6,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
+@EnablePrometheusEndpoint
+@EnableSpringBootMetricsCollector
 public class SpringCloudEurekaApplication {
 
 	public static void main(String[] args) {
